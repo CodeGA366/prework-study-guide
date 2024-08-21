e4a4baf3293e3b349800b918bfe45dca4fc54924
CodeGA366 <codega366@gmail.com>
2024-08-21 14:37:58 -0500

    Added for loops and arrays to the JS file

--- prework-study-guide/script.js
+++ prework-study-guide/script.js
@@ -1,31 +1,42 @@
 const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
-for(let x=0; x < topics.length; x++) {
-    console.log(topics[x])
+const randomTopic = topics[Math.floor(Math.random() * topics.length)]
+
+function listTopics(){
+    for(let x=0; x < topics.length; x++) {
+    console.log(topics[x]);
+    }
 }
 
-/*
-    if (topic === 'HTML') {
+function selectTopic() {
+        if (randomTopic === 'HTML') {
 
-    console.log("Lets study HTML!")
+        console.log("Lets study HTML!")
 
-} else if (topic === 'CSS'){
+    } else if (randomTopic === 'CSS'){
 
-    console.log("Lets study CSS!")
+        console.log("Lets study CSS!")
 
-} else if (topic === 'Git') {
+    } else if (randomTopic === 'Git') {
 
-    console.log("Lets study Git!")
+        console.log("Lets study Git!")
 
-} else if (topic === 'JavaScript') {
+    } else if (randomTopic === 'JavaScript') {
 
-    console.log("Lets study JavaScript!")
+        console.log("Lets study JavaScript!")
 
-} else {
+    } else {
 
-    console.log('Please try again!')
+        console.log('Please try again!')
 
+    }
 }
-*/
 
+console.log('Here are the topics we learned through prework:')
+
+listTopics();
+
+console.log('Which topic should we study first?')
+
+selectTopic();
 
